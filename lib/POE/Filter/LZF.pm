90c5f7e7a0b9a07717ed5f14dca712de478cad96
Chris Williams <chris@bingosnet.co.uk>
2005-07-07 19:53:27 +0000

    Import of BINGOS/POE-Filter-LZF-1.0 from CPAN.
    gitpan-cpan-distribution: POE-Filter-LZF
    gitpan-cpan-version:      1.0
    gitpan-cpan-path:         BINGOS/POE-Filter-LZF-1.0.tar.gz
    gitpan-cpan-author:       BINGOS
    gitpan-cpan-maturity:     released

---
+++ lib/POE/Filter/LZF.pm
@@ -0,0 +1,139 @@
+package POE::Filter::LZF;
+
+use Carp;
+use Compress::LZF qw(compress decompress);
+use vars qw($VERSION);
+
+$VERSION = '1.0';
+
+sub PUT_LITERAL () { 1 }
+
+sub new {
+  my $type = shift;
+  croak "$type requires an even number of parameters" if @_ % 2;
+  my $buffer = { @_ };
+  foreach my $option ( keys %{ $buffer } ) {
+	$buffer->{ lc( $option) } = delete( $buffer->{ $option } );
+  }
+  $buffer->{BUFFER} = [];
+  return bless($buffer, $type);
+}
+
+sub get {
+  my ($self, $raw_lines) = @_;
+  my $events = [];
+
+  foreach my $raw_line (@$raw_lines) {
+	if ( my $line = decompress( $raw_line ) ) {
+		push( @$events, $line );
+	} else {
+		warn "Couldn\'t decompress input\n";
+	}
+  }
+  return $events;
+}
+
+sub get_one_start {
+  my ($self, $raw_lines) = @_;
+
+  foreach my $raw_line (@$raw_lines) {
+	if ( my $line = decompress( $raw_line ) ) {
+		push( @$events, $line );
+	} else {
+		warn "Couldn\'t decompress input\n";
+	}
+  }
+}
+
+sub get_one {
+  my ($self) = shift;
+  my $events = [];
+
+  if ( my $raw_line = shift ( @{ $self->{BUFFER} } ) ) {
+	if ( my $line = decompress( $raw_line ) ) {
+		push( @$events, $line );
+	} else {
+		warn "Couldn\'t decompress input\n";
+	}
+  }
+  return $events;
+}
+
+sub put {
+  my ($self, $events) = @_;
+  my $raw_lines = [];
+
+  foreach my $event (@$events) {
+	if ( my $line = compress( $event ) ) {
+		push( @$raw_lines, $line );
+	} else {
+		warn "Couldn\'t compress output\n";
+	}
+  }
+  return $raw_lines;
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+POE::Filter::LZF -- A POE filter wrapped around Compress::LZF
+
+=head1 SYNOPSIS
+
+    use POE::Filter::LZF;
+
+    my $filter = POE::Filter::LZF->new();
+    my $scalar = 'Blah Blah Blah';
+    my $compressed_array   = $filter->put( [ $scalar ] );
+    my $uncompressed_array = $filter->get( $compressed_array );
+
+    use POE qw(Filter::Stackable Filter::Line Filter::LZF);
+
+    my ($filter) = POE::Filter::Stackable->new();
+    $filter->push( POE::Filter::LZF->new(),
+		   POE::Filter::Line->new( InputRegexp => '\015?\012', OutputLiteral => "\015\012" ),
+
+=head1 DESCRIPTION
+
+POE::Filter::LZF provides a POE filter for performing compression/decompression using L<Compress::LZF|Compress::LZF>. It is
+suitable for use with L<POE::Filter::Stackable|POE::Filter::Stackable>.
+
+=head1 METHODS
+
+=over
+
+=item *
+
+new
+
+Creates a new POE::Filter::LZF object. 
+
+=item *
+
+get
+
+Takes an arrayref which is contains lines of compressed input. Returns an arrayref of decompressed lines.
+
+=item *
+
+put
+
+Takes an arrayref containing lines of uncompressed output, returns an arrayref of compressed lines.
+
+=back
+
+=head1 AUTHOR
+
+Chris Williams <chris@bingosnet.co.uk>
+
+=head1 SEE ALSO
+
+L<POE|POE>
+L<Compress::LZF|Compress::LZF>
+L<POE::Filter::Stackable|POE::Filter::Stackable>
+
+=cut
+
